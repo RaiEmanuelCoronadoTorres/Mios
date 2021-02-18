891ac08a9ff2b017e36718ee3b6b9027b3106988
RaiEmanuelCoronadoTorres <68200500+RaiEmanuelCoronadoTorres@users.noreply.github.com>
2021-02-18 02:48:27 -0500

    mios
    proyectos personales

---
+++ 02_consultor/assets/js/script.js
@@ -0,0 +1,6 @@
+let li = document.querySelector('#menu');
+let icono = document.querySelector('#icono');
+
+    icono.addEventListener('click', function(){
+        menu.classList.toggle('ul-toggle');
+    });
